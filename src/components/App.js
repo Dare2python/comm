6283d4a2c8930d5c9cffbe27969516492c578563
dare2python <31766642+Dare2python@users.noreply.github.com>
2021-12-10 07:43:27 +0200

    First Styled button

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-
+import Button from "components/common/Button";
 
 function App() {
   return (
+    <>
     <h1>
       App
     </h1>
+    <Button>
+      Test
+    </Button>
+    </>
   );
 }
 
